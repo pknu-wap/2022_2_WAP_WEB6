fd7e7cd4c436163d3a08a03585eab7f9138a0b36
hennible0612 <sungwoo166@gmail.com>
2022-10-02 01:51:07 +0900

    init

---
+++ src/main/java/com/web/backend/WebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.web.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProjectApplication.class, args);
+	}
+
+}
