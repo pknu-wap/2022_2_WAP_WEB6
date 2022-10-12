4addf0822d7cd45e9d68a89220965214ec406a85
hennible0612 <sungwoo166@gmail.com>
2022-10-12 16:14:42 +0900

    FEAT : JWT 토큰 추가.

--- src/main/java/com/web/backend/config/SecurityConfiguration.java
+++ src/main/java/com/web/backend/config/SecurityConfiguration.java
@@ -4,12 +4,17 @@ import com.web.backend.service.CustomUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,9 +27,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomUserService userService;
 
+    @Autowired
+    private JWTTokenHelper jwtTokenHelper;
+
+    @Autowired
+    private AuthenticationEntryPoint authenticationEntryPoint;
 
     @Override
-
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //admin 권한 유저 생성
         //memory auth
@@ -39,15 +48,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 //        http.authorizeRequests().anyRequest().permitAll(); //모든 권한 ok
-        http.authorizeRequests().anyRequest().authenticated(); //모든 권한 X
+//        http.authorizeRequests().anyRequest().authenticated(); //모든 권한 X
 
 //        http.authorizeRequests((request) -> request.antMatchers("/h2-console/**")
 //                .permitAll().anyRequest().authenticated()).httpBasic(); //h2-console은 모든 유저가능
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint).and()
+                .authorizeRequests((request) -> request.antMatchers("/api/v1/auth/login").permitAll()
+                        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated())
+                .addFilterBefore(new JWTAuthenticationFilter(userService, jwtTokenHelper),
+                        UsernamePasswordAuthenticationFilter.class);
 
-        http.formLogin(); //로그인 해라
+//        http.formLogin(); //로그인 해라
         http.csrf().disable().headers().frameOptions().disable(); // csrf disable
 //        http.httpBasic(); //제일 기본적인 인증 방법 세션, 쿠키 등이 필요 없다, 로그아웃이 불가능하다.
     }
---
+++ src/main/java/com/web/backend/controller/AuthenticationController.java
@@ -0,0 +1,62 @@
+package com.web.backend.controller;
+
+import com.web.backend.config.JWTTokenHelper;
+import com.web.backend.entity.UserEntity;
+import com.web.backend.requests.AuthenticationRequest;
+import com.web.backend.response.LoginResponse;
+import com.web.backend.response.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.Principal;
+import java.security.spec.InvalidKeySpecException;
+
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin
+public class AuthenticationController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    JWTTokenHelper jwtTokenHelper;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<?> login(@RequestBody AuthenticationRequest authenticationRequest) throws InvalidKeySpecException, NoSuchAlgorithmException {
+        final Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                authenticationRequest.getUserName(), authenticationRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserEntity user = (UserEntity) authentication.getPrincipal();
+        String jwtToken = jwtTokenHelper.generateToken(user.getUsername());
+
+        LoginResponse response = new LoginResponse();
+        response.setToken(jwtToken);
+
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/auth/userinfo")
+    public ResponseEntity<?> getUserInfo(Principal user) {
+        UserEntity userObj = (UserEntity) userDetailsService.loadUserByUsername(user.getName());
+        UserInfo userInfo = new UserInfo();
+        userInfo.setFirstName(userObj.getFirstName());
+        userInfo.setFirstName(userObj.getLastName());
+        userInfo.setRoles(userObj.getAuthorities().toArray());
+
+        return ResponseEntity.ok(userInfo);
+
+    }
+}
