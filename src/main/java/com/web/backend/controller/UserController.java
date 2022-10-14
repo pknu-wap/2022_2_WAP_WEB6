fcf6201f88685ad6114f6856324f336aa86b5cd6
hennible0612 <sungwoo166@gmail.com>
2022-10-14 19:55:19 +0900

    default role 회원가입시 추가

--- src/main/java/com/web/backend/controller/UserController.java
+++ src/main/java/com/web/backend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.web.backend.controller;
 
+import com.web.backend.entity.AuthorityEntity;
 import com.web.backend.entity.UserEntity;
 import com.web.backend.service.UserService;
 import com.web.backend.shared.GenericResponse;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -19,6 +22,9 @@ public class UserController {
 
     @PostMapping("/api/1.0/users")
     GenericResponse createUser(@Valid @RequestBody UserEntity user) { //waiting for user
+        List<AuthorityEntity> authorityEntityList = new ArrayList<>();
+
+        authorityEntityList.add(createAuthority("USER", "User role")); //role default 일반유저
 
         System.out.println("auth login");
         System.out.println(user);
@@ -30,7 +36,18 @@ public class UserController {
         if(user.getUsername() == null){
             throw new UserNotValidException();
         }
+        user.setEnabled(true);
+        user.setAuthorities(authorityEntityList);
+
+
         userService.save(user);
         return new GenericResponse("user saved");
+    }
+    private AuthorityEntity createAuthority(String roleCode, String roleDescription) {
+        AuthorityEntity authority = new AuthorityEntity();
+        authority.setRoleCode(roleCode);
+        authority.setRoleDescription(roleDescription);
+        return authority;
+
     }
 }
