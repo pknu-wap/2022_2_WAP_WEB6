6a4be1cc3afb0560ffb2c5dd66fcfdb5d0d8164f
hennible0612 <sungwoo166@gmail.com>
2022-10-16 22:41:47 +0900

    CHORE : 프론트엔드 Test 파일 삭제

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,24 +1,12 @@
-import {Routes, Route} from "react-router-dom";
 
-import HelloWorld from "./routes/helloworld.component";
-import Login from "./routes/login.component";
-import Test from "./routes/test.component";
 
 function App() {
     return (
-        <Routes>
-            <Route path='/'>
-                <Route path='helloworld' element={<HelloWorld/>}/> {/*react spring 통신 예제 */}
-                <Route path='login' element={<Login/>}/> {/*login 페이지*/}
-                <Route path='test' element={<Test/>}/> {/*login 페이지*/}
+        <div>
 
-                {/*<Route index={true} element={<Home/>}/> /!* index=true이므로 기본*!/*/}
+            <div> HI </div>
 
-
-            </Route>
-
-
-        </Routes>
+        </div>
     );
 }
 export default App;
