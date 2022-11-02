039b9d14aa0e8170a1cd7f429e8bf8707019e4a9
hennible0612 <sungwoo166@gmail.com>
2022-11-03 00:17:15 +0900

    FEAT : TEST

---
+++ src/test/java/com/web/backend/service/ProConTopicServiceTest.java
@@ -0,0 +1,40 @@
+package com.web.backend.service;
+
+import com.web.backend.dto.ProConTopicDto;
+import com.web.backend.entity.ProConTopicEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class ProConTopicServiceTest {
+//    @Autowired
+//    ProConTopicService proConTopicService;
+//
+//    @Test
+//    @Transactional
+//    void create_success() {
+//        // 예상
+//        boolean pro_con = true;
+//        String topic = "create_success";
+//        String content = "create_success_content";
+//        Date due_date = new Date(2022-02-22);
+//        String reason = "create_success_reason";
+//
+//        ProConTopicDto dto = new ProConTopicDto( 0L, pro_con,topic,content,due_date,reason);
+//        ProConTopicEntity expected = new ProConTopicEntity(0L, pro_con,topic,content,due_date,reason);
+//        // 실제
+//        ProConTopicEntity proConTopic = ProConTopicEntity.createProConTopic(dto);
+//
+//
+//        // 비교
+//        assertEquals(expected, proConTopic);
+//
+//    }
+}
