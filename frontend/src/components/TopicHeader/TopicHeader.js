e9869d6d4af992ca455cab731a78db359e4ce703
yunjikwak <mia007478@gmail.com>
2023-01-19 17:21:23 +0900

    feat: due-date 추가

--- frontend/src/components/TopicHeader/TopicHeader.js
+++ frontend/src/components/TopicHeader/TopicHeader.js
@@ -1,11 +1,21 @@
+import { type } from "@testing-library/user-event/dist/type";
 import React from "react";
 import "./TopicHeader.css";
 
 function TopicHeader(props) {
+  let dueDate = props.due_date + "";
+  dueDate = dueDate.replace(/\,/g, "");
+  dueDate = dueDate.split("T");
+
   return (
-    <header className="topicHeader">
-      <span>{props.topic}</span>
-    </header>
+    <>
+      <header className="topicHeader">
+        <span>{props.topic}</span>
+      </header>
+      <span style={{ display: "flex", justifyContent: "flex-end" }}>
+        {dueDate[0]}
+      </span>
+    </>
   );
 }
 
